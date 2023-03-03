38b0931ee4dab61a33bb8368e17478fa7dca2b04
Gauri <gaurimh1999@gmail.com>
2023-03-03 12:10:24 +0530

    Containg information about jenkins

---
+++ info.cpp
@@ -0,0 +1,1 @@
+Beginning with Jenkins 2.335 and Jenkins 2.332.1, the package is configured with systemd rather than the older System V init. See the DigitalOcean community systemd tutorial to better understand the benefits of systemd and the systemctl command.
